d46cf5a6fa732788060b4bdb80834c75a29aa1c9
Alejandro Méndez <victoralemendez@unicauca.edu.co>
2019-11-06 16:19:04 -0500

    Configuración de peticiones en el servidor

---
+++ api/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); // Conviersion a JSON de los datos recibidos por peticiones HTTP
+
+
+// Configuracion de cabeceras http
+app.use(function(request, response, next) {
+     response.header('Access-Control-Allow-Origin', '*');
+     response.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Allow-Request-Method');
+     response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+     response.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+     next();
+ });
+
+
+
+module.exports = app;
